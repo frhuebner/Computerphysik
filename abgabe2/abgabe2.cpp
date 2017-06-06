5498c656f1a256b1e57bbc37051a7d44732100ef
Friedrich Huebner <mailfurmull@gmail.com>
2017-06-06 16:00:44 +0200

    adjust checked_1_1.txt

--- abgabe2/abgabe2.cpp
+++ abgabe2/abgabe2.cpp
@@ -161,13 +161,13 @@ int main(int argc, char** argv){
 		cout << energy << "eV\t" << (v.second?"even":"odd") << endl;  
 	}
 	
-	cout << endl;
+	/*cout << endl;
 	for(auto v: res){
 		//calculate the energy out of z, ie E = (z/a)²*H_BAR²/2/m-V0
 		double energy = (v.first/a)*(v.first/a)*1e18*H_BAR*H_BAR/2/MASS_ELECTRON/CHARGE_ELECTRON-v0*CHARGE_ELECTRON;
 		//
 		cout << v.first << "\t" << v.second << "\t" << (v.second?g(v.first):h(v.first)) << "\t" << energy << endl;  
-	}
+	}*/
 	
 	return 0;
 }
